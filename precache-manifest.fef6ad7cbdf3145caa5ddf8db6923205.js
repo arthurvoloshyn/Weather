9931f7b35927e4b08508ec1f56449c316509d650
Artur <grandfaraon93@gmail.com>
2019-11-21 13:07:12 +0200

    Updates

---
+++ precache-manifest.fef6ad7cbdf3145caa5ddf8db6923205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/Weather/static/media/polygon.cc54c19b.jpg"
+  },
+  {
+    "revision": "4cba2d3fb97fb33ba4c0",
+    "url": "/Weather/static/js/runtime~main.4cba2d3f.js"
+  },
+  {
+    "revision": "108072c4c8f03aec908a",
+    "url": "/Weather/static/js/main.108072c4.chunk.js"
+  },
+  {
+    "revision": "e11c4fa52d92413a99e3",
+    "url": "/Weather/static/js/1.e11c4fa5.chunk.js"
+  },
+  {
+    "revision": "108072c4c8f03aec908a",
+    "url": "/Weather/static/css/main.06979f47.chunk.css"
+  },
+  {
+    "revision": "e11c4fa52d92413a99e3",
+    "url": "/Weather/static/css/1.96b76411.chunk.css"
+  },
+  {
+    "revision": "cfb470674a478fd35bdbcff9807f2503",
+    "url": "/Weather/index.html"
+  }
+];
