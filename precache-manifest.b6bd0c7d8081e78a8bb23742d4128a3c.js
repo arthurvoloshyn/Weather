229b0a0f87ed4d0be6d6ff5fc32e855bf6d17f10
Artur <grandfaraon93@gmail.com>
2019-12-28 17:44:57 +0200

    Updates

---
+++ precache-manifest.b6bd0c7d8081e78a8bb23742d4128a3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/Weather/static/media/polygon.cc54c19b.jpg"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "1505cc3d0094fdd93528",
+    "url": "/Weather/static/js/main.716b7ae4.chunk.js"
+  },
+  {
+    "revision": "e8c7cf154cce7ca6ef84",
+    "url": "/Weather/static/js/2.778868bd.chunk.js"
+  },
+  {
+    "revision": "1505cc3d0094fdd93528",
+    "url": "/Weather/static/css/main.3656966c.chunk.css"
+  },
+  {
+    "revision": "e8c7cf154cce7ca6ef84",
+    "url": "/Weather/static/css/2.21094c67.chunk.css"
+  },
+  {
+    "revision": "5e63bb24b18c11e6ac48b551b466a11e",
+    "url": "/Weather/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Weather/precache-manifest.7a31f52c00f6f8601ee9bccbd676d7eb.js"
+  "/Weather/precache-manifest.b6bd0c7d8081e78a8bb23742d4128a3c.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Weather/index.html"), {
+workbox.routing.registerNavigationRoute("/Weather/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
