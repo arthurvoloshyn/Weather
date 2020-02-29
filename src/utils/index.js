921e18013f6f1554e0df6d22669e1fea3d884f3a
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-02-29 13:43:09 +0200

    Updated date

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,13 @@
+const addPadStart = string => string.padStart(2, '0');
+
 export const kelvinToCelsius = (celsius, round = 1) => (celsius - 273.15).toFixed(round);
 
 export const timeConversion = sec => {
   const date = new Date(sec * 1000);
-  const hours = date.getHours();
-  const modifiedMinutes = `0${date.getMinutes()}`;
-  const modifiedSeconds = `0${date.getSeconds()}`;
-  const minutes = modifiedMinutes.substr(-2);
-  const seconds = modifiedSeconds.substr(-2);
+
+  const seconds = addPadStart(`${date.getSeconds()}`);
+  const minutes = addPadStart(`${date.getMinutes()}`);
+  const hours = addPadStart(`${date.getHours()}`);
 
   return `${hours}:${minutes}:${seconds}`;
 };
